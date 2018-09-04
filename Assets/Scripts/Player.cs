26b14cfe0c0ebb799170eb474b6fba23ad6dbee7
kabirules <javifont@gmail.com>
2018-09-05 00:21:18 +0200

    Chapter 009

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,11 @@ public class Player : MonoBehaviour {
 	public float jumpForce = 10f;
 	public Rigidbody2D circle;
 	public string currentColor;
+	public SpriteRenderer sr;
+	public Color blue;
+	public Color yellow;
+	public Color pink;
+	public Color purple;
 
 	// Use this for initialization
 	void Start () {
@@ -32,15 +37,19 @@ public class Player : MonoBehaviour {
 		{
 			case 0:
 				currentColor = "Blue";
+				sr.color = blue;
 				break;
 			case 1:
 				currentColor = "Yellow";
+				sr.color = yellow;
 				break;
 			case 2:
 				currentColor = "Pink";
+				sr.color = pink;
 				break;
 			case 3:
 				currentColor = "Purple";
+				sr.color = purple;
 				break;								
 		}
 	}
