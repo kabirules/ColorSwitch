126722088da55bf9ff1f3560b4f7331a9c7638d6
kabirules <javifont@gmail.com>
2018-09-05 00:13:08 +0200

    Chapter 008

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,11 @@ public class Player : MonoBehaviour {
 
 	public float jumpForce = 10f;
 	public Rigidbody2D circle;
+	public string currentColor;
 
 	// Use this for initialization
 	void Start () {
-		
+		SetRandomColor();
 	}
 	
 	// Update is called once per frame
@@ -23,5 +24,24 @@ public class Player : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		Debug.Log(collision.tag);
+	}
+
+	void SetRandomColor() {
+		int rand = Random.Range(0, 3);
+		switch (rand) 
+		{
+			case 0:
+				currentColor = "Blue";
+				break;
+			case 1:
+				currentColor = "Yellow";
+				break;
+			case 2:
+				currentColor = "Pink";
+				break;
+			case 3:
+				currentColor = "Purple";
+				break;								
+		}
 	}
 }
