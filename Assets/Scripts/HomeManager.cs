caac2e9d17e6182ea89fbdcbab85ad06a039db9f
kabirules <javifont@gmail.com>
2018-09-20 00:01:03 +0200

    Firebase database testing

--- Assets/Scripts/HomeManager.cs
+++ Assets/Scripts/HomeManager.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -38,13 +41,30 @@ public class HomeManager : MonoBehaviour {
 	/// FIREBASE ///
 	////////////////
 
+    public class HiScore
+    {
+        public string user;
+        public int score;
+        
+        public HiScore(string user, int score)
+        {
+            this.user = user;
+            this.score = score;
+        }
+    }	
+
 	private void FirebaseInit() {
-		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+		FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
 			var dependencyStatus = task.Result;
 			if (dependencyStatus == Firebase.DependencyStatus.Available) {
 				// Set a flag here indiciating that Firebase is ready to use by your
 				// application.
 				Debug.Log("Firebase ready!");
+				FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://mathballs-0000.firebaseio.com/");
+				DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+				HiScore hiscore = new HiScore("Manolo", 100);
+				string json = JsonUtility.ToJson(hiscore);
+				reference.Child("Hiscore").SetRawJsonValueAsync(json);
 			} else {
 				UnityEngine.Debug.LogError(System.String.Format(
 				"Could not resolve all Firebase dependencies: {0}", dependencyStatus));
