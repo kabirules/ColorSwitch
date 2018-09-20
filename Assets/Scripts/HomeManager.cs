1fb34d8c860e575fe3af0464f04dde4c640cd21c
kabirules <javifont@gmail.com>
2018-09-21 00:02:30 +0200

    Adding hiscore - firebase database

--- Assets/Scripts/HomeManager.cs
+++ Assets/Scripts/HomeManager.cs
@@ -62,9 +62,37 @@ public class HomeManager : MonoBehaviour {
 				Debug.Log("Firebase ready!");
 				FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://mathballs-0000.firebaseio.com/");
 				DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
-				HiScore hiscore = new HiScore("Manolo", 100);
-				string json = JsonUtility.ToJson(hiscore);
-				reference.Child("Hiscore").SetRawJsonValueAsync(json);
+				HiScore hiscore1 = new HiScore("Manolo", 100);
+				HiScore hiscore2 = new HiScore("Juanito", 200);
+				HiScore hiscore3 = new HiScore("Rodolfito", 50);
+				List<HiScore> hiscores = new List<HiScore>();
+				hiscores.Add(hiscore1);
+				hiscores.Add(hiscore2);
+				hiscores.Add(hiscore3);
+				string json = JsonUtility.ToJson(hiscore2);
+				// reference.Child("hiscore").SetRawJsonValueAsync(json);
+				string key = reference.Child("hiscore").Push().Key;
+				Dictionary<string, object> hiscore1Dict = new Dictionary<string, object>();
+				hiscore1Dict["user"] = hiscore1.user;
+				hiscore1Dict["score"] = hiscore1.score;
+				Dictionary<string, object> dict = new Dictionary<string, object>();
+				dict["/hiscore/"+key] = hiscore1Dict;
+				reference.UpdateChildrenAsync(dict);
+				// Read data
+				FirebaseDatabase.DefaultInstance
+					.GetReference("hiscore")
+					.GetValueAsync().ContinueWith(task1 => {
+        				if (task1.IsFaulted) {
+          					// Handle the error...
+        				} else if (task1.IsCompleted) {
+							DataSnapshot snapshot = task1.Result;
+							// Do something with snapshot...
+							Debug.Log(snapshot.GetRawJsonValue());
+							HiScore oneHiscore = JsonUtility.FromJson<HiScore>(snapshot.GetRawJsonValue());
+							Debug.Log(oneHiscore.score);
+							Debug.Log(oneHiscore.user);
+						}
+      			});				
 			} else {
 				UnityEngine.Debug.LogError(System.String.Format(
 				"Could not resolve all Firebase dependencies: {0}", dependencyStatus));
